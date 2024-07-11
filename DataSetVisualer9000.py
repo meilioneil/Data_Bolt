def8503db3c6a36c252256aac49b0f8634867ecc
faisalmujawar148 <98691778+faisalmujawar148@users.noreply.github.com>
2024-07-11 18:34:17 +0100

    Create DataSetVisualer9000.py
    This is the barebones code to just hold all the datasets together in 1 spot, and to display all the summer sports (I've listed all the summer sports, instead of the season because I'm dumb)

---
+++ DataSetVisualer9000.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import plotly.express as px #change later
+#from kaggle Olympic Games EDA
+#You'll have to change the paths to the path to the .csv file on your end, when we upload this version on github we can put the path much more easily
+hosts = r'C:\Users\ASUS PC\Downloads\Olympics\olympic_hosts.csv'
+medals = r'C:\Users\ASUS PC\Downloads\Olympics\olympic_medals.csv'
+results = r'C:\Users\ASUS PC\Downloads\Olympics\olympic_results.csv'
+athletes = r'C:\Users\ASUS PC\Downloads\Olympics\olympic_athletes.csv'
+additional_data = r'C:\Users\ASUS PC\Downloads\Olympics\Summer-Olympic-medals-1976-to-2008.csv'
+olympics_hosts = pd.read_csv(hosts)
+olympics_athletes = pd.read_csv(athletes)
+olympics_medals = pd.read_csv(medals)
+olympics_results = pd.read_csv(results)
+additional_olympics_info = pd.read_csv(additional_data,delimiter=",", encoding='latin1')
+additional_olympics_info["Country_Code"].replace({"URS": "RUS",
+                             "ROC": "RUS",
+                             "OAR": "RUS",
+                             "ORS": "RUS",
+                             "GDR": "GER",
+                             "FRG": "GER"}, inplace=True)
+additional_olympics_info["Country"].replace({"Russian Federation": "Russia",
+                        "Soviet Union": "Russia",
+                        "Unified team": "Russia",
+                        "West Germany": "Germany",
+                        "East Germany": "Germany"}, inplace=True)
+df_temp = additional_olympics_info[additional_olympics_info["Sport"].isin(['Aquatics','Archery', 'Athletics', 'Boxing', 'Canoe / Kayak',
+                                                                       'Cycling', 'Equestrian', 'Fencing', 'Gymnastics', 'Judo', 'Modern Pentathlon',
+                                                                        'Rowing', 'Sailing', 'Shooting', 'Weightlifting', 'Wrestling',
+                                                                       'Table Tennis', 'Tennis', 'Badminton', 'Taekwondo', 'Triathlon'])]
+
+# Group and aggregate the filtered data
+df3 = df_temp.groupby(['Sport', 'Country', 'Country_Code', 'Year']).agg(Medal_size=('Medal', 'size')).sort_values('Medal_size', ascending=False).head(100).reset_index()
+
+del df_temp
+
+
+fig = px.histogram(df3, x='Country', y='Medal_size',hover_name='Country',  color='Sport', title='Top Medal winning nations and Sports')
+fig.update_layout(legend_title_text='Sport')
