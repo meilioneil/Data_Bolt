6b3533822a5ba457e143bc3dd1ef37ab19f0da53
faisalmujawar148 <faisal.mujawar4@gmail.com>
2024-07-12 03:25:18 +0100

    flask is not flasking??

--- Webpage/main.py
+++ Webpage/main.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/index')
+def foo():
+    return render_template('index.html')
 
 @app.route('/contact')
 def contact():
@@ -19,4 +22,4 @@ def information():
     return render_template('information.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
